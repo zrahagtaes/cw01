ea735a8442253f7f60ed4a629df723911f34ddab
unknown <zarahgats@gmail.com>
2025-09-20 21:43:09 -0400

    added the image toggle and theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,107 +9,189 @@ void main() {
 }
 
 //Stateless widgets cannot change their state over time.
-
-class MyApp extends StatelessWidget {
+//Making this a Stateful widget in order to have the app be able to change from dark mode to light
+// This widget is the root of your application.
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => MyAppState();
+}
 
-  // This widget is the root of your application.
+//State class for MyApp
+//Stores current theme (light or dark)
+//Provides the toggle function to switch themes
+class MyAppState extends State<MyApp> {
+  //this variable keeps track of the current theme
+  ThemeMode _mode = ThemeMode.light;
+
+  //This method switches between light and dark
+  //setState() makes Flutter rebuild the UI so it shows up
+  void _toggleTheme() {
+    setState(() {
+      _mode = _mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
+  //The build method here makes the overall app structure
+  //Everything is wrapped in MaterialApp (root widget)
+  //This provides user light and dark settings
+  //MyHomePage is the main screen
+  //Gives MyHomePage toggle, function to switch themes, and isDark tells it which theme is active.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Light Dark Theme Demo',
+
+      //In charge of how light mode looks
+      themeMode: _mode,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 231, 36, 227),
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      //In charge of how dark mode looks
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      //Sets first screen user sees
+      //onToggleTheme is the function to switch themes
+      //isDark tells which one is active
+      home: MyHomePage(
+        title: 'Counter and Image Toggle',
+        onToggleTheme: _toggleTheme,
+        isDark: _mode == ThemeMode.dark,
+      ),
     );
   }
 }
 
+//Declares MyHomePage as a StatefulWidget, meaning it has a mutable state
+//The CLASS is immutable though
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  //Constructor for MyHomePage
+  //This is how you create the MyHomePage object
+  //required means you have to provide these values when creating it
+  //refers to the properties of the class, all of these are on the previous block of code
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.onToggleTheme,
+    required this.isDark,
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  //Configuration values for widget
+  //final means that these cannot be changed once widget is created
   final String title;
+  final VoidCallback onToggleTheme;
+  final bool isDark;
 
+  //Connects widget MyHomePage to its mutable state class _MyHomePageState
+  //It actually creates the state object
+  //This is where the actual logic, the changing of variables, and build method() are
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+// we are going to add more comments tmr but for now this is fine
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   int _counter = 0;
+  bool _showFirst = true;
+
+  late final AnimationController _controller;
+  late final Animation<double> _fade;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _fade = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+    _controller.value = 1.0;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    setState(() => _counter++);
+  }
+
+  Future<void> _toggleImage() async {
+    await _controller.reverse();
+    setState(() => _showFirst = !_showFirst);
+    await _controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final imgPath = _showFirst
+        ? 'assets/images/img1.png'
+        : 'assets/images/img2.png';
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: const Text('Increment'),
-            ),
-          ],
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Counter: $_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _incrementCounter,
+                child: const Text('Increment'),
+              ),
+
+              const SizedBox(height: 30),
+
+              FadeTransition(
+                opacity: _fade,
+                child: Image.asset(
+                  imgPath,
+                  width: 300,
+                  height: 300,
+                  fit: BoxFit.contain,
+                ),
+              ),
+
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: _toggleImage,
+                child: const Text('Toggle Image'),
+              ),
+
+              const SizedBox(height: 30),
+
+              OutlinedButton(
+                onPressed: widget.onToggleTheme,
+                child: Text(
+                  widget.isDark
+                      ? 'Switch to Light Mode'
+                      : 'Switch to Dark Mode',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      //No longer needed FloatingActionButton
     );
   }
 }
